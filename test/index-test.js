cc70f9b2f5e565e19a7aa0418cd9b0caba80913d
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-06 04:07:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -26,6 +26,37 @@ Number // 5
 
 leae++Number // 5
 
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
+
+
 number*= 10
 number/= 5
 var number = 10
