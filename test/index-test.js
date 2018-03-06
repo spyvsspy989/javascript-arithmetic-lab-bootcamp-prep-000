fa39dcbc93e9e04da39c4c1d7177cc493c9e0243
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-06 03:58:44 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -38,6 +38,35 @@ divideby3() {
 divideby3()
 console.log(number)
 
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) // 3.333333333335
+ 
+add5()
+ 
+console.log(number) // 8.333333333335
+ 
+// reset number
+number = 10
+ 
+add5()
+ 
+console.log(number) // 15
+ 
+divideBy3()
+ 
+console.log(number) // 5
+
 }
 }
 
