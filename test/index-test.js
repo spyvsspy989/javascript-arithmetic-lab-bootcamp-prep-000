8e6265987369b4c62b6859440f77f362a27c23eb
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-06 03:55:14 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -12,6 +12,15 @@ Number//
 60 - 40 = 
 2 * 3.40 =  
 5.0 / 2.5 = 
+var number = 5
+ 
+number++ // 5... hmmmm
+ 
+number // 6 -- the number was incremented after it was evaluated
+ 
+number-- // 6
+ 
+number // 5
 
 if ('subtract (b, a) is b minus a and returns the result', 
 function() {
